b558ff944b31895065b86a68dd925d84a5a8c984
CLOUDCOOK <96113885+CLOUDCOOKCH@users.noreply.github.com>
2025-09-11 11:22:24 +0200

    test: update msi install path assertion

---
+++ test/commands.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { PSADT_SCENARIOS } = require('../js/commands.js');
+
+const msi = PSADT_SCENARIOS.find(s => s.id === 'msi-install');
+const cmd = msi.build({
+  filePathBase: '$adtSession.DirFiles',
+  filePath: 'app.msi',
+  commonArgs: ['/qn'],
+  transforms: '',
+  argumentList: '',
+  logFileName: ''
+});
+assert.strictEqual(
+  cmd,
+  "Start-ADTMsiProcess -Action Install -FilePath \"$adtSession.DirFiles\\app.msi\" -ArgumentList '/qn'"
+);
+
+console.log('All tests passed.');
